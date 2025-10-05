29c657b1a2701dfd56d6fe8fc42475995149ef92
VireakGumi <chanvireak906@gmail.com>
2025-10-05 11:27:18 +0700

    find problem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+    <!-- Fonts & Icons -->
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'My Website')</title>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Fonts & Icons -->
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,500;1,500&display=swap"
-        rel="stylesheet">
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    rel="stylesheet">
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="{{ asset('fontawesome-free/css/all.min.css') }}">
 
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('style')
 </head>
 
 <body>
@@ -33,11 +32,11 @@
     @include('layouts.footer')
 
     <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('fontawesome-free/js/all.min.js') }}"></script>
     {{-- <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
      --}}
     <!-- Font Awesome JS -->
-    <script src="{{ asset('fontawesome-free/js/all.min.js') }}"></script>
 
     <!-- JS Content -->
 
