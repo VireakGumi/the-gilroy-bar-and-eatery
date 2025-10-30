da41b99ced5126005706eb2f98acccb9ef4fd297
VireakGumi <chanvireak906@gmail.com>
2025-10-31 01:14:16 +0700

    prepare to host

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,6 @@
 <?php
 
-use App\Http\Controllers\CareerController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PrivateEventsController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MenuController;
-use App\Http\Controllers\VenueController;
-use App\Http\Controllers\NavigationController;
-use App\Http\Controllers\DrinkController;
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\WhatOnController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/about-us', function () {
-    return view('about_us');
-})->name('about-us');
-
-
-// old
-Route::get('/ground-floor-area', [VenueController::class, 'groundFloorArea'])->name('ground-floor-area');
-Route::get('/cocktail-dining-area', [VenueController::class, 'cocktailDiningArea'])->name('cocktail-dining-area');
-Route::get('/private-dining', [VenueController::class, 'pravateDining'])->name('private-dining');
-Route::get('/rooftop-terrace', [VenueController::class, 'rooftopTerrace'])->name('rooftop-terrace');
-Route::get('/menu', [MenuController::class, 'index'])->name('menu');
-Route::get('/navigation', [NavigationController::class, 'navigation'])->name('navigation');
-Route::get('/private-events', [PrivateEventsController::class, 'privateEvents'])->name('private-events');
-Route::get('/drink-menu', [DrinkController::class, 'drinkMenu'])->name('drink-menu');
 Route::get('/', [HomeController::class, 'home'])->name('home');
-Route::get('/career', [CareerController::class, 'career'])->name('career');
-
-// New
-Route::get('/about', [AboutController::class, 'about'])->name('about');
-Route::get('/events', [EventController::class, 'index'])->name('events');
-
-//gift_vouchers
-Route::get('/gift_voucher', function (){
-    return view('gift_voucher');
-});
-Route::get('/what-on', [WhatOnController::class, 'index'])->name('what-on');
-
-
